cba91219350ec3af2fd8b24324331537a55867a7
Varghese Thomas <varghesethomase@gmail.com>
2017-11-12 14:43:26 +0530

    design changes moving unwanted text

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,11 +5,9 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import './style.css';
-import UXCard from '../../components/UXComponents/CardGroup';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   /**
@@ -25,14 +23,11 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
           <title>CopraML</title>
           <meta name="description" content="Monsoon is coming" />
         </Helmet>
-        <div className="text-center back-img--things">
-          <div className="copraLogo"><span role="img" aria-label="myran" >🌴</span> Copra<span>ML</span> <i className="flash-div ion-flash mx-3"></i> <span className="wireframe--logo">wireframer</span></div>
+        <div className="text-center back-img-things">
+          <div className="copraLogo"><span className="wireframe-logo">WireFramer</span></div>
           <h1 className="hero-headline">Build App Prototypes using the power of AI.</h1>
-          <Link to="/create" className="btn btn-primary">Create your prototype <i className="ion-chevron-right mx-2"></i></Link>
-        </div>
-        <div className="row justify-content-sm-center ">
-          <div className="col-md-3">
-            <UXCard />
+          <div className="create-project">
+            <Link to="/create" className="btn btn-primary btn-lg">Create your prototype <i className="ion-chevron-right mx-2"></i></Link>
           </div>
         </div>
       </article>
